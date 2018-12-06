bc987665dc00a2db34e5aabe1e593750bde314b4
Data Science Brazil <39283833+datasciencebrazil@users.noreply.github.com>
2018-12-06 16:00:25 -0200

    Algoritmo simples...
    mas com infinitas possibilidades :)

---
+++ algoritmo-rede.py
@@ -0,0 +1,5 @@
+import random
+ids = ["ba","be","bi","bo","bu","ca","ce","ci","co","cu","da","de","di","do","du","fa","fe","fi","fo","fu","ga","ge","gi","go","gu","ja","je","ji","jo","ju","ka","ke","ki","ko","ku","la","le","li","lo","lu","ma","me","mi","mo","mu","na","ne","ni","no","nu","pa","pe","pi","po","pu","qu","qo","ra","re","ri","ro","ru","sa","se","si","so","su","ta","te","ti","to","tu","va","ve","vi","vo","vu","xa","xe","xi","xo","xu","za","ze","zi","zo","zu"]
+a1 = random.choice(ids)
+a2= random.choice(ids)
+print(a1+a2)
